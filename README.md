b696dfa6d671d03307a84b1b3cdf9d15673772b3
1hyzh <mr.ko12@icloud.com>
2025-04-07 16:14:57 +0200

    new

--- README.md
+++
@@ -1,1 +0,0 @@
-For injecting INTLS
--- resource.h
+++ resource.h
@@ -1,10 +1,9 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by Icon.rc
+// Used by Resource.rc
 
 // Next default values for new objects
 // 
-#define IDI_APP_ICON 101
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        101
@@ -13,3 +12,4 @@
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
+#define IDI_APP_ICON 101
